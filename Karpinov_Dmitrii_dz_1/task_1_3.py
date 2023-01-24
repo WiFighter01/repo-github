73f4b1e106bf6206bb7bd7dc45c0dffb24936610
Dmitrii <karpinov92@gmail.com>
2023-01-24 19:49:26 +0300

    finish_dz_1_3

--- Karpinov_Dmitrii_dz_1/task_1_3.py
+++ Karpinov_Dmitrii_dz_1/task_1_3.py
@@ -1,10 +1,14 @@
 def transform_string(number: int) -> str:
     """Возвращает строку вида 'N процентов' с учётом склонения по указанному number"""
-    # место для Вашего кода
-    if n % 10 == 1:
+
+    if n == 11 or n == 12 or n ==13 or n == 14:
+        return f'{n} процентов'
+    elif n % 10 == 1:
         return f'{n} процент'
-
-
+    elif n % 10 == 2 or n % 10 == 3 or n % 10 ==4:
+        return f'{n} процента'
+    else:
+        return f'{n} процентов'
 
 
 for n in range(1, 101):  # по заданию учитываем только значения от 1 до 100
