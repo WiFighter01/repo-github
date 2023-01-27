8379ab142c2b182b4bd0cd7ba93406b77cfcf4f2
Dmitrii <karpinov92@gmail.com>
2023-01-28 00:20:17 +0300

    Алгоритм бинарного поиска.

---
+++ binary_search.py
@@ -0,0 +1,26 @@
+def binary_search(list, item):
+    '''
+    Функция алгоритма бинарного поиска
+    :param list: отсортированный список данных
+    :param item: искомое значение
+    :return: возвращает индекс искомого значение
+    '''
+
+    low = 0
+    high = len(list) - 1
+
+    while low <= high:
+        mid = int((low + high) / 2)
+        guess = list[mid]
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
+
+my_list = [1, 3, 5, 7, 9]
+
+print(binary_search(my_list, 7))
+print(binary_search(my_list, -1))
