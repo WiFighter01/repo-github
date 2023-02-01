1142323c91dbad8f5b78d654700c526aadfa1f88
Dmitrii <karpinov92@gmail.com>
2023-02-01 20:00:59 +0300

    Задание 9_4. Готово.

--- task_9_4.py
+++ task_9_4.py
@@ -1,17 +1,10 @@
 n = int(input())
-a = [['.'] * n for i in range(n)]
+a = [[0] * n for i in range(n)]
 
 for i in range(n):
     for j in range(n):
-        a[i][i] = 0
-        for j in range(i + 1, n):
-            a[i][j] = i + 1
-        for j in range(0, i):
-            a[i][j] = i
-
-
-
-
+        if i != j:
+            a[i][j] = abs(i - j)
 
 for row in a:
     print(' '.join([str(elem) for elem in row]))
